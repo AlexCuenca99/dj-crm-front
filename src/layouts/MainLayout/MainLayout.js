c9a260f2692002af313011414f7841be7ec6307f
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-25 12:16:39 -0500

    Add Navbar component

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Navbar } from '../../components/Main';
+
 export function MainLayout(props) {
 	const { children } = props;
 	return (
 		<div>
-			<h1>MainLayout</h1>
+			<Navbar />
 			{children}
 		</div>
 	);
