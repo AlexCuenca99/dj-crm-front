a65e24f4a1f8b9ec8ac45ee122f222c47b11aafb
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-25 16:17:10 -0500

    Move from tailwind ui to ChakraUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import '@fontsource-variable/open-sans';
+import '@fontsource/poppins';
+
 import React from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import { Navigation } from './routes';
 
+const theme = extendTheme({
+	fonts: {
+		heading: `'Poppins', sans-serif`,
+		body: `'Open Sans Variable', sans-serif`,
+	},
+});
+
 export default function App() {
 	return (
-		<div>
+		<ChakraProvider theme={theme}>
 			<Navigation />
-		</div>
+		</ChakraProvider>
 	);
 }
