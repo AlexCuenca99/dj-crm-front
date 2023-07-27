35b21949ac2167b7f825f0f40e2c6f7765b77b22
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-27 15:06:48 -0500

    Add tags and actions to rows

--- src/components/Main/Leads/LeadsTable/LeadsTable.js
+++ src/components/Main/Leads/LeadsTable/LeadsTable.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import { map } from 'lodash';
+import { Link as ReactRouterLink } from 'react-router-dom';
 import {
+	Link,
 	Table,
 	TableCaption,
 	TableContainer,
+	Tag,
 	Tbody,
 	Td,
 	Th,
 	Thead,
 	Tr,
 } from '@chakra-ui/react';
-import { map } from 'lodash';
-import React from 'react';
 
 export default function LeadsTable(props) {
 	const { leads } = props;
@@ -26,18 +29,31 @@ export default function LeadsTable(props) {
 						<Th>Phone</Th>
 						<Th>Category</Th>
 						<Th>Agent</Th>
+						<Th></Th>
 					</Tr>
 				</Thead>
 				<Tbody>
-					{map(leads, (lead, index) => (
+					{map(leads, (lead, _) => (
 						<Tr key={lead.id}>
 							<Td>{`${lead.first_name} ${lead.last_name}`}</Td>
 							<Td>{lead.email}</Td>
 							<Td>{lead.address}</Td>
+							<Td>{lead.phone}</Td>
+							<Td>
+								<Tag
+									colorScheme={lead.agent ? 'cyan' : 'red'}
+									size="sm"
+								>
+									{lead.agent ? 'Assigned' : 'Unassigned'}
+								</Tag>
+							</Td>
 							<Td>
 								{lead.agent
 									? lead.agent.user.first_name
 									: 'No yet assigned'}
+							</Td>
+							<Td>
+								<Link as={ReactRouterLink}>Edit</Link>
 							</Td>
 						</Tr>
 					))}
