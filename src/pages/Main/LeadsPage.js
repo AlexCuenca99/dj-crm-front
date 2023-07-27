f73399aa2d8b00404021bdc60f2f0848d2afc006
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-27 14:41:56 -0500

    Condition leads render: loading, empy and correct data

--- src/pages/Main/LeadsPage.js
+++ src/pages/Main/LeadsPage.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
+
 import { useLeads } from '../../hooks';
+import { HeaderPage } from '../../components/Main';
+import { isEmpty } from 'lodash';
+import { Alert, AlertIcon, Spinner } from '@chakra-ui/react';
 
 export function LeadsPage() {
 	const { loading, error, leads, getLeads } = useLeads();
@@ -11,8 +15,24 @@ export function LeadsPage() {
 	}, []);
 
 	return (
-		<div>
-			<h1>LeadsPage</h1>
-		</div>
+		<>
+			<HeaderPage
+				title={'Leads'}
+				action={() => console.log('Action 1')}
+				actionTitle={'Create a new lead'}
+				action2={() => console.log('Action 2')}
+				actionTitle2={'Filter by category'}
+			/>
+			{loading ? (
+				<Spinner />
+			) : isEmpty(leads) ? (
+				<Alert status="warning">
+					<AlertIcon />
+					There is not any lead to show
+				</Alert>
+			) : (
+				<h1>Leads table</h1>
+			)}
+		</>
 	);
 }
