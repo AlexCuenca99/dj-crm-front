fe9914dc98f17bbb37bb1b33898caf3a4fac487f
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-27 14:28:51 -0500

    Add actions and titles to Header Page

--- src/components/Main/HeaderPage/HeaderPage.js
+++ src/components/Main/HeaderPage/HeaderPage.js
@@ -1,12 +1,22 @@
-import { Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Spacer, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function HeaderPage(props) {
 	const { title, action, actionTitle, action2, actionTitle2 } = props;
 
 	return (
-		<Heading as="h2" size={'2xl'}>
-			{title}
-		</Heading>
+		<Box>
+			<Flex minWidth="max-content" alignItems="center" gap="2">
+				<VStack align={'start'}>
+					<Heading as="h2" size={'2xl'}>
+						{title}
+					</Heading>
+					<Link onClick={action2}>{actionTitle2}</Link>
+				</VStack>
+				<Spacer />
+				<Link onClick={action}>{actionTitle}</Link>
+			</Flex>
+		</Box>
 	);
 }
