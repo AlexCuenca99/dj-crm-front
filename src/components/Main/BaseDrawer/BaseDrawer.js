5ef96c5a8cae5bc07857376ac1459edfaaff26cf
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-31 12:04:04 -0500

    Add base drawer component

---
+++ src/components/Main/BaseDrawer/BaseDrawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+	Drawer,
+	DrawerBody,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	useDisclosure,
+	Button,
+} from '@chakra-ui/react';
+
+export function BaseDrawer(props) {
+	const { title, isOpen, onClose } = props;
+
+	return (
+		<Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+			<DrawerOverlay />
+			<DrawerContent>
+				<DrawerCloseButton />
+				<DrawerHeader>{title}</DrawerHeader>
+				<DrawerBody></DrawerBody>
+				<DrawerFooter>
+					<Button variant="outline" mr={3} onClick={onClose}>
+						Cancel
+					</Button>
+					<Button colorScheme="blue">Save</Button>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
+	);
+}
