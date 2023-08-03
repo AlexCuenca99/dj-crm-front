6c69f38f3d679e15c29c31c24a4e3e614049f190
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-03 16:14:38 -0500

    Add Header component

--- src/pages/Main/AgentsPage.js
+++ src/pages/Main/AgentsPage.js
@@ -1,5 +1,20 @@
+import { HeaderPage } from 'components/Main';
 import React from 'react';
 
 export function AgentsPage() {
-	return <div>AgentsPage</div>;
+	const addAgent = () => {
+		console.log('Add agent');
+	};
+
+	return (
+		<>
+			<HeaderPage
+				title={'Agents'}
+				actions={addAgent}
+				actionTitle={'Create a new agent'}
+				action2={() => console.log('Action 2')}
+				actionTitle2={'Filter by category'}
+			/>
+		</>
+	);
 }
