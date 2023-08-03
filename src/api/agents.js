1a4ef460e8a875b4d565d3ff34ac90269df7f10a
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-03 16:19:28 -0500

    Add agents get request

---
+++ src/api/agents.js
@@ -0,0 +1,28 @@
+import { valuesIn } from 'lodash';
+import { BASE_API } from 'utils/constants';
+
+export async function getAgentsApi(token) {
+	try {
+		const url = `${BASE_API}/agents/`;
+		const params = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const response = await fetch(url, params);
+
+		if (!response.ok) {
+			const result = await response.json();
+			const errorValues = valuesIn(result);
+			throw new Error(errorValues);
+		}
+
+		const result = await response.json();
+
+		return result;
+	} catch (error) {
+		throw error;
+	}
+}
