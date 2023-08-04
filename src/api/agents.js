e58ff1efa9b54b8af71c63fd93cee38d44aedfc2
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-04 12:12:00 -0500

    Add api call to create a new agent

--- src/api/agents.js
+++ src/api/agents.js
@@ -25,3 +25,28 @@ export async function getAgentsApi(token) {
 		throw error;
 	}
 }
+
+export async function createAgentsApi(token, formValue) {
+	try {
+		const url = `${BASE_API}/agents/`;
+		const params = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(formValue),
+		};
+		const response = await fetch(url, params);
+
+		if (!response.ok) {
+			const result = await response.json();
+			const errorValues = valuesIn(result);
+			throw new Error(errorValues);
+		}
+		const result = await response.json();
+
+		return result;
+	} catch (error) {
+		throw error;
+	}
+}
