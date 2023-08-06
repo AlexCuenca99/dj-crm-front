6201ad2a9252de4054495f65b9f5a019dfaf3e76
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-05 19:06:51 -0500

    Get agents by id API request

--- src/api/agents.js
+++ src/api/agents.js
@@ -56,3 +56,25 @@ export async function createAgentsApi(token, formValue) {
 		throw error;
 	}
 }
+
+export async function getAgentByIdApi(token, id) {
+	try {
+		const url = `${BASE_API}/agents/${id}`;
+		const params = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+		const response = await fetch(url, params);
+
+		if (!response.ok) {
+			const result = await response.json();
+			const errorValues = omit(merge(result, result.errors), ['errors']);
+			throw new Error('Error in request', { cause: errorValues });
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		throw error;
+	}
+}
