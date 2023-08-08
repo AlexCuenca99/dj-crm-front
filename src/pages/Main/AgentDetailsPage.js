551376b84eb4aa449149790426a3cf6ef353433c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-08 18:26:12 -0500

    Add agents form to details

--- src/pages/Main/AgentDetailsPage.js
+++ src/pages/Main/AgentDetailsPage.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useAgent } from 'hooks';
+import { AddEditAgentForm } from 'components/Main';
 
 export function AgentDetailsPage() {
 	const { loading, agent, error, getAgentById } = useAgent();
 
 	let { id } = useParams();
 
+	const [formik, setFormik] = useState(null);
+	const [formLoading, setFormLoading] = useState(false);
+	const [refetch, setRefetch] = useState(false);
+
+	const onRefetch = () => {
+		setRefetch((prev) => !prev);
+	};
+
 	useEffect(() => {
 		getAgentById(id);
 	}, []);
 
-	console.log(agent);
 	return (
-		<div>
-			<h1>Agent Details</h1>
-		</div>
+		<>
+			<AddEditAgentForm
+				agent={agent}
+				setFormik={setFormik}
+				// onCloseDrawer={drawerDisclosure.onClose}
+				// onCloseAlertDialog={alertDialogDisclosure.onClose}
+				// setShowToast={setShowToast}
+				setFormLoading={setFormLoading}
+				// setToastTitle={setToastTitle}
+				// setToastDescription={setToastDescription}
+				// setToastStatus={setToastStatus}
+				// setToastDuration={setToastDuration}
+				// setToastIsClosable={setToastIsClosable}
+				onRefetch={onRefetch}
+			/>
+		</>
 	);
 }
