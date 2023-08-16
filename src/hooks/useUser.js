9e3b64faf2500e81efa6edf6204d16d2666ed421
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-16 17:51:27 -0500

    Add request all users method to custom hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,6 +1,15 @@
-import { getMeApi, verifyTokenApi } from '../api/users';
+import { useState } from 'react';
+
+import { useAuth } from 'hooks';
+import { getMeApi, verifyTokenApi, getAllUsersApi } from '../api/users';
 
 export function useUser() {
+	const { auth } = useAuth();
+
+	const [users, setUsers] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
 	const getMe = async (token) => {
 		try {
 			const response = await getMeApi(token);
@@ -18,8 +27,27 @@ export function useUser() {
 			throw error;
 		}
 	};
+
+	const getAllUsers = async () => {
+		try {
+			setLoading(true);
+			const response = await getAllUsersApi(auth.token);
+			setUsers(response);
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			setError(error);
+
+			throw error;
+		}
+	};
 	return {
+		users,
+		loading,
+		error,
+
 		getMe,
 		verifyToken,
+		getAllUsers,
 	};
 }
