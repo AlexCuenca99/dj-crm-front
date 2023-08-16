b3818d254307768641269aa83464f2789e73be1b
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-16 14:43:12 -0500

    Add leads category options

--- src/utils/feeders.js
+++ src/utils/feeders.js
@@ -3,3 +3,11 @@ export const genderOptions = [
 	{ key: 1, text: 'Female', value: 'F' },
 	{ key: 2, text: 'Other', value: 'O' },
 ];
+
+export const leadsCategoryOptions = [
+	{ key: 0, text: 'New', value: 'LST' },
+	{ key: 1, text: 'Lost', value: 'ASG' },
+	{ key: 2, text: 'Assigned', value: 'CNT' },
+	{ key: 3, text: 'Contacted', value: 'CVR' },
+	{ key: 4, text: 'Unassigned', value: 'USG' },
+];
