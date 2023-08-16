7964577326d11d36793fca381f728a8b7de30b09
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-16 11:54:45 -0500

    Add my leads api request

--- src/api/agents.js
+++ src/api/agents.js
@@ -78,3 +78,25 @@ export async function getAgentByIdApi(token, id) {
 		throw error;
 	}
 }
+
+export async function getMyLeadsApi(token) {
+	try {
+		const url = `${BASE_API}/agents/my-leads/`;
+		const params = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+		const response = await fetch(url, params);
+
+		if (!response.ok) {
+			const result = await response.json();
+			const errorValues = omit(merge(result, result.errors), ['errors']);
+			throw new Error('Error in request', { cause: errorValues });
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		throw error;
+	}
+}
