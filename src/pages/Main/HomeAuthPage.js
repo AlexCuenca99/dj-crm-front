a1694d06f2bf868d508508dc10410728eac73cfd
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-16 18:26:42 -0500

    Filter information about users and leads

--- src/pages/Main/HomeAuthPage.js
+++ src/pages/Main/HomeAuthPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { map } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { filter, map } from 'lodash';
 import { MdSupervisedUserCircle } from 'react-icons/md';
 import {
 	Card,
@@ -13,39 +13,62 @@ import {
 } from '@chakra-ui/react';
 
 import { TitleLabelIconCard } from 'components/Main';
+import { useUser, useLeads } from 'hooks';
 
-const cardItems = [
-	{
-		title: 'Organizers',
-		mainLabel: '10',
-		subLabel: 'Leads',
-		icon: MdSupervisedUserCircle,
-	},
-	{
-		title: 'Agents',
-		mainLabel: '10',
-		subLabel: 'Leads',
-		icon: MdSupervisedUserCircle,
-	},
-	{
-		title: 'Leads',
-		mainLabel: '10',
-		subLabel: 'Leads',
-		icon: MdSupervisedUserCircle,
-	},
-	{
-		title: 'Assigned',
-		mainLabel: '10',
-		subLabel: 'Leads',
-		icon: MdSupervisedUserCircle,
-	},
-];
 export function HomeAuthPage() {
+	const { loading, getAllUsers } = useUser();
+	const { getLeads } = useLeads();
+	const [orgUsers, setOrgUsers] = useState(0);
+	const [agtUsers, setAgtUsers] = useState(0);
+	const [leadUsers, setLeadUsers] = useState(0);
+	const [assignedLeads, setAssignedLeads] = useState(0);
+
+	useEffect(() => {
+		getAllUsers().then((res) => {
+			setAgtUsers(filter(res, { role: 'AGT' }).length);
+			setOrgUsers(filter(res, { role: 'ORG' }).length);
+		});
+		getLeads().then((res) => {
+			setLeadUsers(res.length);
+			setAssignedLeads(
+				filter(res, (currRes) => currRes.agent !== null).length
+			);
+		});
+	}, []);
+
+	const cardItems = [
+		{
+			title: 'Organizers',
+			mainLabel: orgUsers,
+			subLabel: 'ORG',
+			icon: MdSupervisedUserCircle,
+		},
+		{
+			title: 'Agents',
+			mainLabel: agtUsers,
+			subLabel: 'AGT',
+			icon: MdSupervisedUserCircle,
+		},
+		{
+			title: 'Leads',
+			mainLabel: leadUsers,
+			subLabel: 'Leads',
+			icon: MdSupervisedUserCircle,
+		},
+		{
+			title: 'Assigned',
+			mainLabel: assignedLeads,
+			subLabel: 'ASG',
+			icon: MdSupervisedUserCircle,
+		},
+	];
+
 	return (
 		<>
 			<SimpleGrid columns={{ base: 1, sm: 1, md: 3, lg: 4 }} spacing={10}>
 				{map(cardItems, (cardItem, index) => (
 					<TitleLabelIconCard
+						key={index}
 						title={cardItem.title}
 						mainLabel={cardItem.mainLabel}
 						subLabel={cardItem.subLabel}
