9d1b2bdc033164ce9adb31aa70ee21b19c8e2231
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-16 17:44:39 -0500

    Add cards to dashboard

--- src/pages/Main/HomeAuthPage.js
+++ src/pages/Main/HomeAuthPage.js
@@ -1,5 +1,92 @@
 import React from 'react';
+import { map } from 'lodash';
+import { MdSupervisedUserCircle } from 'react-icons/md';
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Button,
+	Heading,
+	SimpleGrid,
+	Text,
+} from '@chakra-ui/react';
 
+import { TitleLabelIconCard } from 'components/Main';
+
+const cardItems = [
+	{
+		title: 'Organizers',
+		mainLabel: '10',
+		subLabel: 'Leads',
+		icon: MdSupervisedUserCircle,
+	},
+	{
+		title: 'Agents',
+		mainLabel: '10',
+		subLabel: 'Leads',
+		icon: MdSupervisedUserCircle,
+	},
+	{
+		title: 'Leads',
+		mainLabel: '10',
+		subLabel: 'Leads',
+		icon: MdSupervisedUserCircle,
+	},
+	{
+		title: 'Assigned',
+		mainLabel: '10',
+		subLabel: 'Leads',
+		icon: MdSupervisedUserCircle,
+	},
+];
 export function HomeAuthPage() {
-	return <div>HomeAuthPage</div>;
+	return (
+		<>
+			<SimpleGrid columns={{ base: 1, sm: 1, md: 3, lg: 4 }} spacing={10}>
+				{map(cardItems, (cardItem, index) => (
+					<TitleLabelIconCard
+						title={cardItem.title}
+						mainLabel={cardItem.mainLabel}
+						subLabel={cardItem.subLabel}
+						icon={cardItem.icon}
+					/>
+				))}
+			</SimpleGrid>
+			<SimpleGrid
+				columns={{ base: 1, sm: 1, md: 2, lg: 2 }}
+				spacing={10}
+				mt="7"
+			>
+				<Card align="center">
+					<CardHeader>
+						<Heading size="md"> Customer dashboard</Heading>
+					</CardHeader>
+					<CardBody>
+						<Text>
+							View a summary of all your customers over the last
+							month.
+						</Text>
+					</CardBody>
+					<CardFooter>
+						<Button colorScheme="blue">View here</Button>
+					</CardFooter>
+				</Card>
+				<Card align="center">
+					<CardHeader>
+						<Heading size="md"> Customer dashboard</Heading>
+					</CardHeader>
+					<CardBody>
+						<Text>
+							View a summary of all your customers over the last
+							month.
+						</Text>
+					</CardBody>
+					<CardFooter>
+						<Button colorScheme="blue">View here</Button>
+					</CardFooter>
+				</Card>
+			</SimpleGrid>
+		</>
+	);
 }
