ec2f6b76f737ebd1ccbbeac79a911d5974b83c98
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-17 18:55:31 -0500

    Error 404 design

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,12 @@
+import { Center, Heading, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export function Error404() {
 	return (
-		<div>
-			<h1>Error404</h1>
-		</div>
+		<Center h="100vh" flexDirection="column">
+			<Image src="/svgs/undraw-building-block.svg" w="50vh" mb="7" />
+			<Heading>This page is under construction</Heading>
+			<Text fontSize="xl">We are working on it!</Text>
+		</Center>
 	);
 }
