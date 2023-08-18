6aa876dcf594fa363ddd6c797dd034e1eab8be24
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-17 19:05:34 -0500

    Design forbidden page

--- src/components/Main/Forbidden/Forbidden.js
+++ src/components/Main/Forbidden/Forbidden.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import {
-	AbsoluteCenter,
-	Alert,
-	AlertDescription,
-	AlertIcon,
-	AlertTitle,
-	Button,
-	Center,
-} from '@chakra-ui/react';
+import { Button, Center, Heading, Image, Text } from '@chakra-ui/react';
 
 import './Forbidden.scss';
 
@@ -20,27 +12,17 @@ export function Forbidden() {
 	};
 
 	return (
-		<Center h="calc(100vh - 160px)">
-			<Alert
-				status="warning"
-				variant="subtle"
-				flexDirection="column"
-				alignItems="center"
-				justifyContent="center"
-				textAlign="center"
-			>
-				<AlertIcon boxSize="40px" mr={0} />
-				<AlertTitle mt={4} mb={1} fontSize="lg">
-					We are sorry
-				</AlertTitle>
-				<AlertDescription maxWidth="sm">
-					The page you are looking for is restricted for your role.
-					Please refer to your system administrator
-				</AlertDescription>
-				<Button colorScheme="teal" onClick={handleNavigate} mt="5">
-					Go to home
-				</Button>
-			</Alert>
+		<Center h="calc(100vh - 160px)" flexDirection="column">
+			<Image src="/svgs/undraw-warning.svg" w="50vh" mb="7" />
+			<Heading>We are sorry</Heading>
+			<Text>
+				The page you are looking for is restricted for your role. Please
+				refer to your system administrator
+			</Text>
+
+			<Button colorScheme="teal" onClick={handleNavigate} mt="5">
+				Go to home
+			</Button>
 		</Center>
 	);
 }
